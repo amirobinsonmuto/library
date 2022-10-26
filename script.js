a99610284094e82095f5e871f53de91a0eb4bede
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-10-26 06:05:33 -0700

    Add form fields and basic fuctions
    JS gets the values from the form fields, creates a new object using the object constructor, and adds (pushes) to the empty array.

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-const test = document.getElementById(1);
+let myLibrary = [];
 
-test.classList.add('bluebackground');
+let submitBtn = document.getElementById('submit');
+
+//object constructor
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read; //yes or no
+}
+
+function addBookToLibrary() {
+
+    let form_author = document.getElementById('author').value;
+    let form_title = document.getElementById('title').value;
+    let form_pages = parseInt(document.getElementById('pages').value);
+    let form_read = document.getElementById('read').value;
+
+    const newBook = new Book(form_author, form_title, form_pages, form_read);
+
+    // const newBook = new Book("JK", "Harry", 98, "yes");
+
+    myLibrary.push(newBook);
+
+    console.log(myLibrary);
+
+    return myLibrary;
+}
+
+submitBtn.addEventListener('click', () => {
+    addBookToLibrary();
+});
+
+
+
+
+
 
