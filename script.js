284229aa1202674a3936142dd2645821e4b2f957
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-10-27 06:50:06 -0700

    Fix AddBookToLibrary function to use newBook object more in the code

--- script.js
+++ script.js
@@ -21,66 +21,70 @@ function Book(author, title, pages, read) {
 //creates an object using the constructor and adds to the myLibrary array
 function addBookToLibrary() {
 
+    //get the values from the form input
     let form_author = document.getElementById('author').value;
     let form_title = document.getElementById('title').value;
     let form_pages = parseInt(document.getElementById('pages').value);
     let form_read = document.getElementById('read').checked;
 
+    //create a newBook object using a constructor
     const newBook = new Book(form_author, form_title, form_pages, form_read);
 
+    console.log(newBook);//delete later
+
     console.log(myLibrary);//delete later
+
+    //add a new object to myLibrary array
     myLibrary.push(newBook);
 
-    //function to create a div and append the obj Book properties as child
-    function createCard () {
+    //create DOM elements and text content
+    const div_card = document.createElement('div');
+    div_card.classList.add('card');
+    const p_author = document.createElement('p');
+    const p_title = document.createElement('p');
+    const p_pages = document.createElement('p');
+    const p_read = document.createElement('p');
+    p_author.textContent = newBook.author;
+    p_title.textContent = newBook.title;
+    p_pages.textContent = newBook.pages;
+    p_read.textContent = newBook.read;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('deleteBtn');
+    deleteBtn.textContent = 'Delete';
 
-        //create DOM elements and nodes
-        const div_card = document.createElement('div');
-        div_card.classList.add('card');
-        const p_author = document.createElement('p');
-        const p_title = document.createElement('p');
-        const p_pages = document.createElement('p');
-        const p_read = document.createElement('p');
-        p_author.textContent = form_author;
-        p_title.textContent = form_title;
-        p_pages.textContent = form_pages;
-        p_read.textContent = form_read;
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add('deleteBtn');
-        deleteBtn.textContent = 'Delete';
+    //append nodes to the elements
+    div_card.appendChild(p_author);
+    div_card.appendChild(p_title);
+    div_card.appendChild(p_pages);
+    div_card.appendChild(p_read);
+    div_card.appendChild(deleteBtn);
+    cardSection.appendChild(div_card);
 
-        //append nodes to the elements
-        div_card.appendChild(p_author);
-        div_card.appendChild(p_title);
-        div_card.appendChild(p_pages);
-        div_card.appendChild(p_read);
-        div_card.appendChild(deleteBtn);
-        cardSection.appendChild(div_card);
+    //set data-attribute with myLibrary index number
+    let myLibraryIndex = myLibrary.indexOf(newBook);
+    console.log(myLibraryIndex);//to delete later
+    div_card.setAttribute('data-attribute', myLibraryIndex);
 
-        //set data-attribute with myLibrary index number
-        let myLibraryIndex = myLibrary.length-1;
-        div_card.setAttribute('data-attribute', myLibraryIndex);
+    //select delete buttons
+    deleteBtns = document.querySelectorAll('.deleteBtn');
 
-        //select delete buttons
-        deleteBtns = document.querySelectorAll('.deleteBtn');
-
-        //delete btn function
-        deleteBtns.forEach((deleteBtn) => {
-            deleteBtn.addEventListener('click', (e) => {
-                let index = e.target.parentElement.getAttribute('data-attribute');
-                console.log(index);
-            })
+    //delete btn function
+    deleteBtns.forEach((deleteBtn) => {
+        deleteBtn.addEventListener('click', (e) => {
+            let index = e.target.parentElement.getAttribute('data-attribute');
+            console.log(index);
         })
-    }
+    })
 
-    createCard(myLibrary[myLibrary.length-1]);
-    
 }
 
-//execute addBookToLibrary function with a button click
-submitBtn.addEventListener('click', () => {
-    addBookToLibrary();
-});
+//execute addBookToLibrary function to all array elements with a button click
+function displayBooks () {
+    myLibrary.forEach(addBookToLibrary());
+}
+
+submitBtn.addEventListener('click', displayBooks)
+
 
 
 
