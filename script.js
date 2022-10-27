465e7bf4f261fe28c142ac5f9a846b29b83b4461
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-10-27 08:19:49 -0700

    Add removeCards function and displayAllCards function
    Add removeCards function and displayAllCards function and attached them to the submit button along side with addBookToLibrary. Make some changes to the delete button, however, it's work in progress

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//global arrays and variables
 let myLibrary = [];
 let submitBtn = document.getElementById('submit');
 let form = document.getElementById('form');
@@ -18,7 +19,7 @@ function Book(author, title, pages, read) {
     this.read = read; 
 }
 
-//creates an object using the constructor and adds to the myLibrary array
+//function to create an object, and adds it to the myLibrary array
 function addBookToLibrary() {
 
     //get the values from the form input
@@ -36,7 +37,10 @@ function addBookToLibrary() {
 
     //add a new object to myLibrary array
     myLibrary.push(newBook);
+}
 
+//function to create a card 
+function createCard (el) {
     //create DOM elements and text content
     const div_card = document.createElement('div');
     div_card.classList.add('card');
@@ -44,10 +48,10 @@ function addBookToLibrary() {
     const p_title = document.createElement('p');
     const p_pages = document.createElement('p');
     const p_read = document.createElement('p');
-    p_author.textContent = newBook.author;
-    p_title.textContent = newBook.title;
-    p_pages.textContent = newBook.pages;
-    p_read.textContent = newBook.read;
+    p_author.textContent = el.author;
+    p_title.textContent = el.title;
+    p_pages.textContent = el.pages;
+    p_read.textContent = el.read;
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('deleteBtn');
     deleteBtn.textContent = 'Delete';
@@ -61,9 +65,7 @@ function addBookToLibrary() {
     cardSection.appendChild(div_card);
 
     //set data-attribute with myLibrary index number
-    let myLibraryIndex = myLibrary.indexOf(newBook);
-    console.log(myLibraryIndex);//to delete later
-    div_card.setAttribute('data-attribute', myLibraryIndex);
+    div_card.setAttribute('data-attribute', myLibrary.indexOf(el));
 
     //select delete buttons
     deleteBtns = document.querySelectorAll('.deleteBtn');
@@ -73,17 +75,33 @@ function addBookToLibrary() {
         deleteBtn.addEventListener('click', (e) => {
             let index = e.target.parentElement.getAttribute('data-attribute');
             console.log(index);
+            // myLibrary.splice(index, 1);
+            // console.log(myLibrary);
+            // removeCards();
+            // displayAllCards();
         })
     })
 
 }
 
-//execute addBookToLibrary function to all array elements with a button click
-function displayBooks () {
-    myLibrary.forEach(addBookToLibrary());
+//function to remove all DOM elements under .card_section
+//instruction from https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
+function removeCards () {
+while (cardSection.firstChild) {
+  cardSection.removeChild(cardSection.firstChild);
+}}
+
+//function to display all elements(book objects) in the myLibrary array 
+function displayAllCards () {
+    myLibrary.forEach(el => createCard(el));
 }
 
-submitBtn.addEventListener('click', displayBooks)
+//eventlistner click for the submit button to invoke three functions above
+submitBtn.addEventListener('click', () => {
+    addBookToLibrary();
+    removeCards();
+    displayAllCards();
+})
 
 
 
